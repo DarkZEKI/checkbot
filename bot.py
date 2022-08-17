5448e69d473a9ce2c7aa520c68db7cf404eb2aec
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2022-08-17 17:09:05 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,8 +39,8 @@ BOT_ID = bot_info.id
 
 # USE YOUR ROTATING PROXY API IN DICT FORMAT http://user:pass@providerhost:port
 proxies = {
-           'http': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/',
-           'https': 'http://qnuomzzl-rotate:4i44gnayqk7c@p.webshare.io:80/'
+           'http': 'http://pjendloh:jej0xowxfrz5@p.webshare.io:7658/',
+           'https': 'http://pjendloh:jej0xowxfrz5@p.webshare.io:7612/'
 }
 
 session = requests.Session()
